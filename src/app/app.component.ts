5841da08e37e9c6b18a7589cad0b6e39a5b8562a
Eduardo Xavier <49372451+Eduardo-XavierPaula@users.noreply.github.com>
2024-04-20 19:45:30 -0300

    ✨ New feature: Estudando ngIf e ngFor, by Eduardo Xavier

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,9 +6,48 @@ import { Component } from '@angular/core';
   styleUrl: './app.component.scss',
 })
 export class AppComponent {
+  // two_way_data_binding
   name: string = 'Felipe';
 
   handleInputChange(event: string) {
     console.log(event);
+  }
+
+  // NgIf
+  minhaProp = true;
+  toggleProp() {
+    this.minhaProp = !this.minhaProp;
+  }
+
+  isTruthy() {
+    return {};
+    //   return [];
+    //   return true;
+    //   return 1;
+    //   return "teste";
+    //   return "0";
+    //   return "false";
+  }
+  isFalsy() {
+    return false;
+    return 0;
+    return '';
+    return null;
+    return undefined;
+    return -0;
+    return NaN;
+  }
+
+  // NgFor
+  personSelectedIndex: number | undefined;
+  listPeople = [
+    { name: 'Fulano de Tal', age: 10 },
+    { name: 'Fulano de Tal', age: 20 },
+    { name: 'Fulano de Tal', age: 30 },
+    { name: 'Fulano de Tal', age: 40 },
+  ];
+
+  selectPerson(index: number) {
+    this.personSelectedIndex = index;
   }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,9 +4,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FormsModule } from '@angular/forms';
+import { PersonComponent } from './person/person.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, PersonComponent],
   imports: [BrowserModule, AppRoutingModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent],
