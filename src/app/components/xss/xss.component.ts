af57ccf389bd83d23475a68a9a71c2108ca299c3
Eduardo Xavier <49372451+Eduardo-XavierPaula@users.noreply.github.com>
2024-05-09 22:34:07 -0300

    ✨ New feature: Finalizando o estudo de XSS, by Eduardo Xavier

--- src/app/components/xss/xss.component.ts
+++ src/app/components/xss/xss.component.ts
@@ -1,4 +1,4 @@
-import { Component, ElementRef } from '@angular/core';
+import { Component, ElementRef, Renderer2 } from '@angular/core';
 
 @Component({
   selector: 'app-xss',
@@ -6,17 +6,31 @@ import { Component, ElementRef } from '@angular/core';
   styleUrl: './xss.component.scss',
 })
 export class XssComponent {
-  exemplo: string =
-    '<img src="teste.jpg" onerror="alert(\'imagem não encontrada\')">';
-  constructor(private readonly _elRef: ElementRef) {}
+  exemplo: string = '<img src="teste.jpg" onerror="alert(\'Ataque XSS\')">';
+  constructor(
+    private readonly _elRef: ElementRef,
+    private readonly _renderer2: Renderer2
+  ) {}
 
   createElement(inputText: string) {
     const divEl = document.createElement('div');
+    divEl.classList.add('bg-gray');
     divEl.innerHTML = inputText;
     const container = this._elRef.nativeElement.querySelector(
       '.xss'
     ) as HTMLDivElement;
 
     container.append(divEl);
+  }
+  createElementCorrect(inputText: string) {
+    const divEl = this._renderer2.createElement('div');
+    const text = this._renderer2.createText(inputText);
+    this._renderer2.appendChild(divEl, text);
+    this._renderer2.setStyle(divEl, 'color', 'blue');
+    this._renderer2.addClass(divEl, 'bg-gray');
+    const container = this._elRef.nativeElement.querySelector(
+      '.xss'
+    ) as HTMLDivElement;
+    this._renderer2.appendChild(container, divEl);
   }
 }
